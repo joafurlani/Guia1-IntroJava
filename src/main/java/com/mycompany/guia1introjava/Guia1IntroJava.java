2f59f9525dacb71c631a17d3c9fc20a59fde653c
Joaquin <joa.furlani@gmail.com>
2023-09-26 20:23:25 +0200

    Primer commit

---
+++ src/main/java/com/mycompany/guia1introjava/Guia1IntroJava.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.guia1introjava;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Guia1IntroJava {
+
+    public static void main(String[] args) {
+       // Escribir un programa que pida dos números enteros por teclado y calcule la suma de los dos. El programa deberá después mostrar el resultado de la suma
+       
+       
+    }
+}
